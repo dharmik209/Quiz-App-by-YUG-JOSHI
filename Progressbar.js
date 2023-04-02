50d109b0f106c0db9dcdfa95470ab5cf1c7191a8
Yug Joshi <117895325+dharmik209@users.noreply.github.com>
2023-04-02 16:37:10 +0530

    Update Progressbar.js

--- Progressbar.js
+++ Progressbar.js
@@ -1,1 +1,20 @@
+import React, {useState, useEffect} from 'react'
 
+export default function Progressbar() {
+    const [filled, setFilled] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+  return (
+    <div>
+        <div className="Progressbar">
+            <div style={{
+                hieght: "100%",
+                width: '${filled}%',
+                backgroundColor: "#a66cff",
+                transition: "width 0.5s"
+            }}></div>
+            <span>{ filled }%</span>
+      </div>
+      <button className="btn" onClick={() => setIsRunning(true)}>Run</button>
+    </div>
+  )
+}
